c3b0c590d9db9adf8d6664c36f2c2869caaf86e5
Ayoub Gaouet <gaouetayoub1@gmail.com>
2026-02-22 16:00:52 +0100

    Remove unused findSupplierByIdCategory method from SupplierService and SupplierRepository

--- src/main/java/com/ayoub/pmsapp/repository/SupplierRepository.java
+++ src/main/java/com/ayoub/pmsapp/repository/SupplierRepository.java
@@ -4,5 +4,4 @@ import com.ayoub.pmsapp.entities.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-    Supplier findFirstBySupplierCategory_Id(long id);
 }
--- src/main/java/com/ayoub/pmsapp/service/Impl/SupplierServiceImpl.java
+++ src/main/java/com/ayoub/pmsapp/service/Impl/SupplierServiceImpl.java
@@ -6,7 +6,6 @@ import com.ayoub.pmsapp.entities.Supplier;
 import com.ayoub.pmsapp.repository.SupplierCategoryRepository;
 import com.ayoub.pmsapp.repository.SupplierRepository;
 import com.ayoub.pmsapp.service.SupplierService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +14,11 @@ import java.util.List;
 public class SupplierServiceImpl implements SupplierService {
     private SupplierRepository supplierRepository;
 
-    @Autowired
-    private SupplierCategoryRepository supplierCategoryRepository;
+    private final SupplierCategoryRepository supplierCategoryRepository;
 
-    public SupplierServiceImpl(SupplierRepository supplierRepository) {
+    public SupplierServiceImpl(SupplierRepository supplierRepository, SupplierCategoryRepository supplierCategoryRepository) {
         this.supplierRepository = supplierRepository;
+        this.supplierCategoryRepository = supplierCategoryRepository;
     }
 
     @Override
@@ -31,13 +30,6 @@ public class SupplierServiceImpl implements SupplierService {
     public Supplier findSupplierById(long id) {
         return supplierRepository.findById(id).orElseThrow(() -> new RuntimeException("Supplier not found with id: " + id));
     }
-
-
-    @Override
-    public Supplier findSupplierByIdCategory(long id) {
-        return supplierRepository.findFirstBySupplierCategory_Id(id);
-    }
-
     @Override
     public Supplier saveSupplier(SupplierDTO supplierDTO) {
         Supplier supplier = new Supplier();
