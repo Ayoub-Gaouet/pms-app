a2176beb12f392d601a7dc53f97a2f536b348625
Ayoub Gaouet <gaouetayoub1@gmail.com>
2026-03-04 22:11:32 +0100

    Add CRUD operations for product categories: implement save, update, and delete methods in ProductCategoryRestController

--- src/main/java/com/ayoub/pmsapp/controllers/ProductCategoryRestController.java
+++ src/main/java/com/ayoub/pmsapp/controllers/ProductCategoryRestController.java
@@ -2,6 +2,7 @@ package com.ayoub.pmsapp.controllers;
 
 import com.ayoub.pmsapp.entities.ProductCategory;
 import com.ayoub.pmsapp.service.ProductCategoryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +17,31 @@ public class ProductCategoryRestController {
         this.productCategoryService = productCategoryService;
     }
 
-    @RequestMapping(method= RequestMethod.GET)
+    @GetMapping
     public List<ProductCategory> getAllCategories()
     {
         return productCategoryService.getAllCategories();
     }
-    @RequestMapping(value="/{id}",method = RequestMethod.GET)
+
+    @GetMapping("/{id}")
     public ProductCategory findCategoryById(@PathVariable("id") Long id) {
         return productCategoryService.findCategoryById(id);
     }
 
+    @PostMapping
+    public ProductCategory saveCategory(@RequestBody ProductCategory productCategory) {
+        return productCategoryService.saveCategory(productCategory);
+    }
+
+    @PutMapping("/{id}")
+    public ProductCategory updateCategory(@PathVariable Long id, @RequestBody ProductCategory productCategory) {
+        productCategory.setId(id);
+        return productCategoryService.updateCategory(productCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long id) {
+        productCategoryService.deleteCategory(id);
+    }
 }
